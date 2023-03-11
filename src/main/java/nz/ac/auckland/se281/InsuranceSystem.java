aac10f76949b56cd75d5a14a2214f7ff5da5bc59
Joshua Tan <jtan678@aucklanduni.ac.nz>
2023-03-11 22:21:27 +1300

    Passed all T1 test cases

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,25 +1,38 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  // Might need to remove
+  ArrayList<String> databaseNames = new ArrayList<String>();
+  ArrayList<String> databaseAge = new ArrayList<String>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-   
-    // ArrayList<Users> listUsers = new ArrayList<Users>()
   }
 
   public void printDatabase() {
 
-    // int numberProfiles = 0;
-    // array list for database
+    int numberUsers = databaseNames.size();
 
-    // System.out.println("Database has 0 profiles.");
-    MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+    if (numberUsers == 0) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+      return;
+    }
 
-    // MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage();
+    if (numberUsers == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+    } else {
+      String numberUsersStr = String.valueOf(numberUsers);
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(numberUsersStr, "s", ":");
+    }
 
-    // MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+    // Print out list of users
+    for (int i = 0; i < numberUsers; i++) {
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
+          String.valueOf(i + 1), databaseNames.get(i), databaseAge.get(i));
+    }
 
     // MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberProfiles),
     // "s", ":");
@@ -27,28 +40,27 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: convert age to integer
-    // Check if profile already exists
-    // Check if username is at least 3 characters
-    // MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
     // Check what error to give if both name and age are wrong
 
-
-    //Convert name to title case
-
-    // String tempName = userName;
-    // String lowerName = tempName.toLowerCase();
-    // create-profile HJKABFnkonfJKOAEF 10 
+    // Convert user name to title case
     userName = userName.substring(0, 1).toUpperCase() + userName.substring(1).toLowerCase();
+
+    // Check if profile already exists
+    if (databaseNames.contains(userName)) {
+      MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+      return;
+    }
+
+    // Check if length of user name is at least 3 characters
     if (userName.length() >= 3) {
 
       int intAge = Integer.parseInt(age); // What the heck does this do though?
 
+      // Check if age is a positive integer. If it is, add the information to the database
       if (intAge > 0) {
         MessageCli.PROFILE_CREATED.printMessage(userName, age);
-
-
-
+        databaseNames.add(userName);
+        databaseAge.add(age);
       } else {
         MessageCli.INVALID_AGE.printMessage(age, userName);
       }
@@ -56,6 +68,9 @@ public class InsuranceSystem {
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
     }
 
+    // DELETE LATER
+    // System.out.println(databaseNames);
+    // System.out.println(databaseAge);
   }
 
   public void loadProfile(String userName) {
