f1dbad76f7af87ae1298f718d5526f9132ca9730
Joshua Tan <jtan678@aucklanduni.ac.nz>
2023-03-11 20:07:49 +1300

    Added some logic for detecting user name validity. Passed T1_04

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -8,12 +8,41 @@ public class InsuranceSystem {
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+
+    // int numberProfiles = 0;
+    // array list for database
+
     System.out.println("Database has 0 profiles.");
+
+    MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage();
+
+    // MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+
+    // MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberProfiles),
+    // "s", ":");
+
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+    // TODO: convert age to integer
+    // Check if profile already exists
+    // Check if username is at least 3 characters
+    // MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+    // Check what error to give if both name and age are wrong
+
+    if (userName.length() >= 3) {
+
+      int intAge = Integer.parseInt(age); // What the heck does this do though?
+
+      if (intAge > 0) {
+        MessageCli.PROFILE_CREATED.printMessage(userName, age);
+      } else {
+        MessageCli.INVALID_AGE.printMessage(age, userName);
+      }
+    } else {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    }
+
   }
 
   public void loadProfile(String userName) {
